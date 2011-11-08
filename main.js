58f3acc6029b549aefd0c680e3e03b9fd602a6a6
Roy Q <roybotnik@gmail.com>
2011-11-08 14:04:07 -0500

    updated demo to work with updated framework..

--- main.js
+++ main.js
@@ -9,13 +9,19 @@ var routerConfig = require('./config.js');
 var controller = require('./controllers/test.js').Instance;
 routerConfig.controllers = {test: controller};
 
+routerConfig.logger = new Framework.Core.Logger();
+
 // Get router
 var router = new Framework.Core.Router(routerConfig);
+
+// Get logger
+var logger = new Framework.Core.Logger();
 
 // Get server
 var server = new Framework.Core.Http.Server({
 	http : http,
-	router : router
+	router : router,
+	logger : logger
 });
 
 server.start("127.0.0.1", 1337);
